cdaed04ef878a3e8af48bce3b4d6257d1da85fdb
GuilhermeBarroso-sys <guibarrosodeoliveira5@gmail.com>
2022-03-04 19:40:34 -0300

    fix porting

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(process.env.PORT || 3000);   
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log("Server is running!");
+});

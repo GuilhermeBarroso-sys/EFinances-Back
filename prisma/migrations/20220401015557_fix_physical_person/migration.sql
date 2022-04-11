20bd2fd771348d5364ea07f1d803ff90d9d106fe
GuilhermeBarroso <202010422@unifoa.edu.br>
2022-04-11 11:56:03 -0300

    heroku

--- prisma/migrations/20220401015557_fix_physical_person/migration.sql
+++ prisma/migrations/20220401015557_fix_physical_person/migration.sql
@@ -3,6 +3,7 @@
 
   - You are about to drop the column `physical_person` on the `accounts` table. All the data in the column will be lost.
 
+
 */
 -- AlterTable
 ALTER TABLE `accounts` DROP COLUMN `physical_person`;
